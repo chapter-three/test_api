509334d09f2c56648746b01826c0d25fdc1c2397
Minnur Yunusov <mmyunusov@gmail.com>
2016-04-18 12:22:25 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Custom RESTful API in Drupal 8
+
+This is a very simple module that demonstrates implementation of a custom RESTful API in Drupal 8
+
+The module example supports GET/POST/PUT/DELETE methods
---
+++ src/Controller/TestAPIController.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\test_api\Controller\TestAPIController.
+ */
+
+namespace Drupal\test_api\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller routines for test_api routes.
+ */
+class TestAPIController extends ControllerBase {
+
+  /**
+   * Callback for `my-api/get.json` API method.
+   */
+  public function get_example( Request $request ) {
+
+  	$response['data'] = 'Some test data to return';
+    $response['method'] = 'GET';
+
+    return new JsonResponse( $response );
+  }
+
+  /**
+   * Callback for `my-api/put.json` API method.
+   */
+  public function put_example( Request $request ) {
+
+    $response['data'] = 'Some test data to return';
+    $response['method'] = 'PUT';
+
+    return new JsonResponse( $response );
+  }
+
+  /**
+   * Callback for `my-api/post.json` API method.
+   */
+  public function post_example( Request $request ) {
+
+    // This condition checks the `Content-type` and makes sure to 
+    // decode JSON string from the request body into array.
+    if ( 0 === strpos( $request->headers->get( 'Content-Type' ), 'application/json' ) ) {
+      $data = json_decode( $request->getContent(), TRUE );
+      $request->request->replace( is_array( $data ) ? $data : [] );
+    }
+
+    $response['data'] = 'Some test data to return';
+    $response['method'] = 'POST';
+
+    return new JsonResponse( $response );
+  }
+
+  /**
+   * Callback for `my-api/delete.json` API method.
+   */
+  public function delete_example( Request $request ) {
+
+    $response['data'] = 'Some test data to return';
+    $response['method'] = 'DELETE';
+
+    return new JsonResponse( $response );
+  }
+
+}
